8846928cf148d3d666c50a231fff3cb98ee8815f
unknown <marine.richer41@gmail.com>
2023-10-30 20:46:57 +0100

    input

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,10 +19,17 @@ namespace InventLab
            
         }
 
+        private String _message;
+
+        public String Message
+        {
+            get => _message;
+            set => _message = value;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
-          
+            MessageBox.Show(this.Message);
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -34,14 +42,11 @@ namespace InventLab
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-           
-        }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            _message = textBox1.Text;
         }
     }
 }
